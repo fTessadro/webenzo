7c6fc19eeb0bfd8b609b5177159e2255b491e1ae
fTessadro <federicotessadro@gmail.com>
2025-12-13 17:48:20 -0300

    fix: set base path for correct GitHub Pages routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
  site: 'https://ftessadro.github.io',
-  base: 'webenzo',
+  base: '/webenzo/',
   vite: {
     plugins: [tailwindcss()],
   },
