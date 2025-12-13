c3a417052ad9fd43d3af456547f232802a891829
fTessadro <federicotessadro@gmail.com>
2025-12-13 16:33:49 -0300

    feat: add github actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+
 // https://astro.build/config
 export default defineConfig({
+ site: 'https://ftessadro.github.io',
+  base: 'webenzo',
   vite: {
     plugins: [tailwindcss()],
   },
+  
 });
