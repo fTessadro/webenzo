0e866ee9ca5f01767539c0070c0d5f226ddabdd5
fTessadro <federicotessadro@gmail.com>
2026-03-07 20:37:27 -0300

    agrego sitemap y dominio final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
- site: 'https://ftessadro.github.io',
-  base: '/webenzo/',
+  //site: 'https://ftessadro.github.io',
+  site: 'https://www.innovadentalstudio.com.ar',
   vite: {
     plugins: [tailwindcss()],
   },
-  
+  integrations: [sitemap()],
 });
